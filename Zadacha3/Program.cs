85c56726736d75f24398283fe4ea4a747348dd6b
Admin <tmurzagaleev@yandex.ru>
2022-08-07 17:06:32 +0700

    Решение задачи 6 из ДЗ

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на 2 без остатка).
+
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number % 2 == 0) Console.WriteLine(Number + " является четным числом ");
+else Console.WriteLine(Number + " является нечетным числом ");
+
