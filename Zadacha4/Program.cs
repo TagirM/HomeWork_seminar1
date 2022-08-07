6b7312086672808cf31165db01e72c3052ee29bf
Admin <tmurzagaleev@yandex.ru>
2022-08-07 17:18:26 +0700

    Решение задачи 8 из ДЗ

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int n = 1;
+
+System.Console.WriteLine("Четные числа от 1 до заданного: ");
+
+while (n <= Number)
+{
+    if (n % 2 == 0) System.Console.WriteLine(n);
+    n++;
+}
+
